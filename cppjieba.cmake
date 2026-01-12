ba2a989492004252368c4e312a71d4b0bcc4925f
Huiyicc <1925473403@qq.com>
2026-01-12 23:53:26 +0800

    初始化暂存

--- cppjieba.cmake
+++ cppjieba.cmake
@@ -1,10 +1,15 @@
 include_guard(GLOBAL)
 include(${CMAKE_CURRENT_LIST_DIR}/base.cmake)
 
+if(NOT TARGET cppjieba)
+    cppmodule_add_subdirectory(cppjieba "${CPPMODULE_ROOTPATH}/cppjieba")
+endif()
+
 if(NOT TARGET cppmodule::cppjieba)
     add_library(cppmodule::cppjieba INTERFACE IMPORTED GLOBAL)
     target_include_directories(cppmodule::cppjieba INTERFACE 
         "${CPPMODULE_ROOTPATH}/cppjieba/include"
 #        "${CPPMODULE_ROOTPATH}/cppjieba/deps/limonp/include"
     )
+    target_link_libraries(cppmodule::cppjieba INTERFACE cppjieba)
 endif()
