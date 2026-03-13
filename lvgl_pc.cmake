e92e6dddefc49611f8398528c5162cc410224147
Huiyicc <1925473403@qq.com>
2026-03-13 17:52:39 +0800

    lvgl补全

--- lvgl_pc.cmake
+++ lvgl_pc.cmake
@@ -32,28 +32,45 @@ set(LVGL_CFG_USE_WAYLAND 0)
 set(LVGL_CFG_USE_NUTTX 0)
 set(LVGL_CFG_USE_LINUX_FBDEV 0)
 
+# LVGLEx 项目根目录（cmake/ 的上一级）
+get_filename_component(LVGLEX_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+
 # 平台模板选择
-if(WIN32)
-    set(TEMPLATE_FILE "lv_cong_template_win.h.in")
-elseif(ANDROID)
-    # TODO: Android 模板
-elseif(UNIX)
-    # TODO: Linux 模板
+if(NOT DEFINED LVGL_TEMPLATE_FILE)
+    if(WIN32)
+        set(LVGL_TEMPLATE_FILE "${LVGLEX_ROOT_DIR}/privately/lv_cong_template_win.h.in")
+    elseif(ANDROID)
+        # TODO: Android 模板
+    elseif(UNIX)
+        # TODO: Linux 模板
+    endif()
 endif()
 
 # ==============================================================================
 # 构建 LVGL
 # ==============================================================================
 if(NOT TARGET lvgl)
-    # 根据平台模板生成 lv_conf.h
-    if(DEFINED TEMPLATE_FILE)
-        set(LVGL_CFG_TEMPLATE_HEADER "${CMAKE_SOURCE_DIR}/privately/${TEMPLATE_FILE}")
-        configure_file(${LVGL_CFG_TEMPLATE_HEADER} "${CMAKE_SOURCE_DIR}/lv_conf.h" @ONLY)
+    # 根据平台模板生成 lv_conf.h 到 third_party/ 目录
+    if(DEFINED LVGL_TEMPLATE_FILE AND EXISTS "${LVGL_TEMPLATE_FILE}")
+        configure_file("${LVGL_TEMPLATE_FILE}" "${CPPMODULE_ROOTPATH}/lv_conf.h" @ONLY)
     else()
-        message(WARNING "[CppModule] No LVGL config template defined for this platform")
+        message(WARNING "[CppModule] No LVGL config template defined for this platform: LVGL_TEMPLATE_FILE=${LVGL_TEMPLATE_FILE}")
     endif()
 
+    # 告诉 LVGL 的 CMake 去正确的位置找 lv_conf.h
+    set(LV_BUILD_CONF_PATH "${CPPMODULE_ROOTPATH}/lv_conf.h" CACHE PATH "" FORCE)
+
     cppmodule_add_subdirectory(lvgl "${CPPMODULE_ROOTPATH}/lvgl")
+
+    # LVGL 编译 SDL/FreeType 驱动时需要对应头文件路径
+    if(TARGET lvgl)
+        if(TARGET SDL2-static)
+            target_link_libraries(lvgl PRIVATE SDL2::SDL2-static)
+        endif()
+        if(TARGET freetype)
+            target_link_libraries(lvgl PRIVATE freetype)
+        endif()
+    endif()
 endif()
 
 if(NOT TARGET cppmodule::lvgl)
