85737cbf90226f2e293aabd61ccc89dd156b59ec
Huiyicc <1925473403@qq.com>
2026-06-01 15:59:52 +0800

    修复cmake

--- LVGLEx.cmake
+++ LVGLEx.cmake
@@ -1,5 +1,12 @@
-CHECK_SUB(CPPMODULE_LVGL)
-include_directories(${CPPMODULE_ROOTPATH}/LVGLEx/include)
-add_subdirectory(${CPPMODULE_ROOTPATH}/LVGLEx ${CPPMODULE_BINARY_SUBDIR}/LVGLEx)
-set(CPPMODULE_LINK_LIBRARIES_ALL ${CPPMODULE_LINK_LIBRARIES_ALL} LVGLEx-static)
-set(CPPMODULE_LVGLEX LVGLEx-static)
+include_guard(GLOBAL)
+include(${CMAKE_CURRENT_LIST_DIR}/base.cmake)
+
+if(NOT TARGET lvgl_ex)
+  cppmodule_add_subdirectory(lvgl_ex "${CPPMODULE_ROOTPATH}/LVGLEx")
+endif()
+
+if(NOT TARGET cppmodule::lvgl_ex)
+  add_library(cppmodule::lvgl_ex INTERFACE IMPORTED GLOBAL)
+  target_include_directories(cppmodule::lvgl_ex INTERFACE ${CPPMODULE_ROOTPATH}/LVGLEx/include)
+  target_link_libraries(cppmodule::lvgl_ex INTERFACE LVGLEx-static)
+endif()
